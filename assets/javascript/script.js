c4f1b073dcdc2c7f1b93bc0763672d53c7bad052
Daniel <87837294+Danx0921@users.noreply.github.com>
2023-06-14 15:08:36 -0300

    atualizado javascript do menu
    antes não estava pegando a imagem no javascript da funcionalidade fechar, agora está concertado.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,9 +2,13 @@ function menuShow() {
     let menuMobile = document.querySelector('.mobile-menu');
     if (menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open');
-        document.querySelector('.icon').src = "Ciências da Computação/projeto bjj/menu_white_36dp.svg";
+        // document.querySelector('.icon').src = "Ciências da Computação/projeto bjj/menu_white_36dp.svg";
+        document.querySelector('.icon').src = "/assets/img/menu_white_36dp.svg";
     } else {
         menuMobile.classList.add('open')
-        document.querySelector('.icon').src = "Ciências da Computação/projeto bjj/close_white36dp.svg";
+        // document.querySelector('.icon').src = "Ciências da Computação/projeto bjj/close_white36dp.svg";
+        document.querySelector('.icon').src = "/assets/img/close_white_36dp.svg";
+
+
     }
 }
