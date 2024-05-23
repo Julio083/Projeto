5901b2b873e66683e2a5db368175db33e5f15a20
Daniel Dantas <daniel.dantas808@gmail.com>
2024-05-23 14:23:40 -0300

    Pasta dos front end separado e adicionado arquivo com navbar e o header

---
+++ front-end-novo/src/js/frequencia.js
@@ -0,0 +1,4 @@
+// preciso de uma função que receba como argumento o mês a ser visualizado e o ano
+// preciso de uma variável que me conte que mês e ano é da data atual
+// preciso descobrir quantos dias tem um mês para mostrar na tela do front end
+
