cb8dd51f444d83b1aeb8b250ed90a54ec851f029
Daniel Dantas <daniel.dantas808@gmail.com>
2024-05-24 14:36:41 -0300

    Feito um esboço da frequencia html e começado os testes no frequencia.js

--- front-end-novo/src/js/frequencia.js
+++ front-end-novo/src/js/frequencia.js
@@ -1,4 +1,7 @@
-// preciso de uma função que receba como argumento o mês a ser visualizado e o ano
+// preciso de uma função que receba como argumento o mês a ser visualizado e o ano e retorne o número de dias criando um objeto de uma classe do tipo falta
+// a classe falta vai ter uma 
 // preciso de uma variável que me conte que mês e ano é da data atual
-// preciso descobrir quantos dias tem um mês para mostrar na tela do front end
 
+
+const teste = new Date(2024, 0,0).getDate() // pega o dia anterior do mês de janeiro de 2024 no caso o último dia de 2023
+console.log(teste)
